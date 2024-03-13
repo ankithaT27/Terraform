b731f73b8a39b80f57aec4feb68050cc581fa297
ankithaT27 <104780117+ankithaT27@users.noreply.github.com>
2024-03-13 18:46:32 +0530

    Update CompleteEc2.tf

--- day2/CompleteEc2.tf
+++ day2/CompleteEc2.tf
@@ -11,5 +11,14 @@ provider "aws"{
 region = 'us-east-1"
 }
 
-resource aws
+resource "aws_key_pair" "mykeypair"{
+key_name="terraformkey
+public_key= file("/home/ubuntu/. shh/terraformkey.pub")
+
+resource "aws_instance" "myec2"{
+key=aws_key_pair.mykeypair.key_name
+
+}
+
+
 
