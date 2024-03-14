123ea802555222e12818c53fcb378005ef04cb62
ankithaT27 <104780117+ankithaT27@users.noreply.github.com>
2024-03-14 17:23:01 +0530

    Update main.tf

--- day3/main.tf
+++ day3/main.tf
@@ -5,19 +5,6 @@ region="us-east1-"
 
 
 
-
-variable amiid{
-description="provide ami"
-
-}
-
-variable instance_type{
-description="provide instance_type"
-
-}
-
-
-
 resource "aws_instance" "my_ec2"{
 ami = var.amiid
 instance_type=var.instance_type
