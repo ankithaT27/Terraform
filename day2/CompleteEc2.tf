90a092980d06dd562797d4be8ca043db2f455052
ankithaT27 <104780117+ankithaT27@users.noreply.github.com>
2024-03-14 11:30:34 +0530

    Update CompleteEc2.tf

--- day2/CompleteEc2.tf
+++ day2/CompleteEc2.tf
@@ -11,21 +11,47 @@ provider "aws"{
 region = 'us-east-1"
 }
 
-resource "aws_key_pair" "mykeypair"{
-key_name="terraformkey"
-public_key= file("/home/ubuntu/. shh/terraformkey.pub")
-}
+#resource "aws_key_pair" "mykeypair"{
+#key_name="terraformkey"
+#public_key= file("/home/ubuntu/. shh/terraformkey.pub")
+#}
 
 resource "aws_default_vpc" "default_vpc"{
 }
 
+resource "aws_security_group" "my_Securitygroup"{
+name= "allow ssh"
+description ="allow shh traffic from developers"
+
+vpc_id = aws_default_vpc.default_vpc_id
+ingress{
+description="TLS from VPC"
+protocol="TCP"
+from_port=22
+to_port=22
+
+#allow traffic from ip
+cidr_blocks = ["0.0.0.0/0"]
+
+}
+
+tags{
+name="ssh"
+}
+}
 
 
-
+variable "my_ami_id"{
+type= string
+default=" "
+}
 
 
 resource "aws_instance" "myec2"{
-key=aws_key_pair.mykeypair.key_name
+#key=aws_key_pair.mykeypair.key_name
+ami_id=var.my_ami_id
+instance_type=t2.micro
+security_groups=[aws_security_group.my_Securitygroup]
 
 }
 
